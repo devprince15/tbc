57b3048ed8ca12ca4e5ae9be8b0a6801a99dae75
prince <devprince.aipl@gmail.com>
2024-09-16 13:54:50 +0530

    new changes

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+Author: Javed Ur Rehman
+Website: https://www.allphptricks.com
+*/
+//Stripe Credentials Configuration
+define("STRIPE_SECRET_API_KEY", "sk_test_51PvDHBGRWr3Y84uWK1zsX8CsnYi0B80bo8LSxMmvZWLHQkxDYQzD8iAsr9t71qISkYJdFxJxVYzKsWfVeitAGWeO00hSdPDmWC");
+define("STRIPE_PUBLISHABLE_KEY", "pk_test_51PvDHBGRWr3Y84uWWE7i8b3p1TvMDUeex8C6oCrQGuBgoQz10voaqVKWkkfcsdro1pdUQwqGWCa7J7WPZS2UkSI300XMeiUJIt");
+
+//Sample Product Details
+define('CURRENCY', 'USD');
+define('AMOUNT', '10');
+define('DESCRIPTION', 'Laptop Bag');
+
+// Database Credentials Configuration 
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'Your Database Name');
+define('DB_USERNAME', 'Your Database Username');
+define('DB_PASSWORD', 'Your Database Password');
+?>
